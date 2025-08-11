b3c039a189e053969d153cedc8298b8a99646ce9
ansarirayyan01 <ansarirayyan874@gmail.com>
2025-08-11 18:16:46 +0530

    added theme service in the app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { UsersService } from './services/users.service';
 import { User } from './interfaces/Users';
-import {
-  RouterLink,
-  RouterLinkActive,
-  RouterOutlet,
-  Router,
-  NavigationEnd,
-} from '@angular/router';
+import { RouterOutlet, Router } from '@angular/router';
 import { NgIf } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
-import { AddUserComponent } from "./add-user/add-user.component";
-import { GetUsersComponent } from "./get-users/get-users.component";
-
+import { ThemeService } from './services/theme.service';
 
 @Component({
   selector: 'app-root',
@@ -22,11 +14,16 @@ import { GetUsersComponent } from "./get-users/get-users.component";
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   userList: User[] = [];
   title = 'crud-app';
 
-  constructor(private userService: UsersService, public router: Router) {}
+  constructor(
+    private userService: UsersService,
+    public router: Router,
+    private themeService: ThemeService,
+    private renderer: Renderer2
+  ) {}
 
   showHeader(): boolean {
     const url = this.router.url;
@@ -39,6 +36,19 @@ export class AppComponent {
 
   ngOnInit() {
     this.getUsers();
+    this.themeService.darkMode$.subscribe((isDarkMode) => {
+      // console.log("Dark mode status:", isDarkMode);
+      if (isDarkMode) {
+        this.renderer.addClass(document.body, 'dark-theme');
+      } else {
+        this.renderer.removeClass(document.body, 'dark-theme');
+      }
+    });
+  }
+
+  toggleDarkMode() {
+    // console.log("Toggling dark mode");
+    this.themeService.toggleDarkMode();
   }
 
   getUsers() {
