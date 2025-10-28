d7ac6acb257e9307e7993854728efb1ddd8bdea4
ansarirayyan01 <ansarirayyan874@gmail.com>
2025-10-28 14:39:27 +0530

    added spinner for loading

--- src/app/spinner/spinner.component.ts
+++ src/app/spinner/spinner.component.ts
@@ -1,12 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { SpinnerService } from '../services/spinner.service';
 
 @Component({
   selector: 'app-spinner',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './spinner.component.html',
   styleUrl: './spinner.component.css'
 })
 export class SpinnerComponent {
+  constructor(public spinnerService: SpinnerService) {}
 
 }
